fe9783ffb168aadf789e5d87b9e763081c1f36ec
ArtemZemlyakov <139268188+ArtemZemlyakov@users.noreply.github.com>
2023-07-12 23:27:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const carName = 'BMW';
+let maxCarSpeed = '360';
+let carOwner = 'Artem';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = '280'
+console.log('у машины изменилась скорость',maxCarSpeed);
+
+carOwner = 'Mark';
+console.log('у машины изменился владелец',carOwner);
+
